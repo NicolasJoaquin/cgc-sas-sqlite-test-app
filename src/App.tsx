cdeb1e13fc5a1c3858f90e1383b70f47de937e97
Nicolas Diorio <nicolas.diorio@naiades.com.ar>
2023-11-16 15:42:14 -0300

    declaring the Singleton SQLite Hook in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,10 @@ import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 
+/* SQLite */
+import { SQLiteHook, useSQLite } from 'react-sqlite-hook';
+import React, { useState } from 'react';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -34,43 +38,85 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+interface JsonListenerInterface {
+  jsonListeners: boolean,
+  setJsonListeners: React.Dispatch<React.SetStateAction<boolean>>,
+}
+interface existingConnInterface {
+  existConn: boolean,
+  setExistConn: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+// Singleton SQLite Hook
+export let sqlite: any;
+// Existing Connections Store
+export let existingConn: any;
+// Is Json Listeners used
+export let isJsonListeners: JsonListenerInterface;
+
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  /* Inicialización del Singleton de SQLite para luego ser utilizado en otros componentes */
+  const [existConn, setExistConn] = useState(false);
+  existingConn = {existConn: existConn, setExistConn: setExistConn};
+  
+  sqlite = useSQLite();
+  console.log(`$$$ in App sqlite.isAvailable  ${sqlite.isAvailable} $$$`);
+
+  const {echo, getPlatform, createConnection, closeConnection,
+    retrieveConnection, retrieveAllConnections, closeAllConnections,
+    addUpgradeStatement, importFromJson, isJsonValid, copyFromAssets,
+    isAvailable, saveToStore} = useSQLite();
+
+  sqlite = {echo: echo, getPlatform: getPlatform,
+    createConnection: createConnection,
+    closeConnection: closeConnection,
+    retrieveConnection: retrieveConnection,
+    retrieveAllConnections: retrieveAllConnections,
+    closeAllConnections: closeAllConnections,
+    addUpgradeStatement: addUpgradeStatement,
+    importFromJson: importFromJson,
+    isJsonValid: isJsonValid,
+    copyFromAssets: copyFromAssets,
+    isAvailable:isAvailable, saveToStore: saveToStore};
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/tab1">
+              <Tab1 />
+            </Route>
+            <Route exact path="/tab2">
+              <Tab2 />
+            </Route>
+            <Route path="/tab3">
+              <Tab3 />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/tab1" />
+            </Route>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="tab1" href="/tab1">
+              <IonIcon aria-hidden="true" icon={triangle} />
+              <IonLabel>Tab 1</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab2" href="/tab2">
+              <IonIcon aria-hidden="true" icon={ellipse} />
+              <IonLabel>Tab 2</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab3" href="/tab3">
+              <IonIcon aria-hidden="true" icon={square} />
+              <IonLabel>Tab 3</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+    </IonApp>
+  );  
+}
 
 export default App;
